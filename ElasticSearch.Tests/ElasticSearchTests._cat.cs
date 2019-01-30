fa0f52ab496d82bc37c3dbde16e6f349141d3698
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-01-30 14:22:37 -0800

    :microscope: GetServerClusterNodes_Test passes

--- ElasticSearch.Tests/ElasticSearchTests._cat.cs
+++ ElasticSearch.Tests/ElasticSearchTests._cat.cs
@@ -15,6 +15,28 @@ namespace ElasticSearch.Tests
         [ProjectFileData(typeof(ElasticSearchTests),
             new[]
             {
+                @"json\GetServerClusterNodes_Test.json"
+            },
+            numberOfDirectoryLevels: 3)]
+        public async Task GetServerClusterNodes_Test(FileSystemInfo ioFile)
+        {
+            var j = GetIoJObject(ioFile);
+            var uri = GetInputUri(j["input"]["uri"]);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            request.Headers.Clear();
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(MimeTypes.ApplicationJson));
+
+            var response = await GetServerResponseAsync(request);
+            j["output"] = JArray.Parse(response);
+
+            File.WriteAllText(ioFile.FullName, j.ToString());
+        }
+
+        [Theory]
+        [ProjectFileData(typeof(ElasticSearchTests),
+            new[]
+            {
                 @"json\GetServerHealth_Test.json"
             },
             numberOfDirectoryLevels: 3)]
