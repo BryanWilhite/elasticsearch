783eb90a75dfa8f86740aea5c71c3f8133c258b6
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-01-30 16:22:10 -0800

    :microscope: PostCustomerPainlessById_Test passes

--- ElasticSearch.Tests/ElasticSearchTests._index.cs
+++ ElasticSearch.Tests/ElasticSearchTests._index.cs
@@ -117,12 +117,10 @@ namespace ElasticSearch.Tests
         }
 
         /// <summary>
-        /// POSTs a new customer in an index generated on the fly (when it does not exist).
+        /// POSTs an update to the specified customer generated
+        /// in <see cref="ElasticSearchTests.PutCustomerInNewIndex_Test(FileSystemInfo)"/>.
         /// </summary>
         /// <param name="ioFile"></param>
-        /// <remarks>
-        /// Running this POST repeatedly will add multiple documents with auto-generated IDs.
-        /// </remarks>
         [Theory]
         [ProjectFileData(typeof(ElasticSearchTests),
             new[]
@@ -131,6 +129,35 @@ namespace ElasticSearch.Tests
             },
             numberOfDirectoryLevels: 3)]
         public async Task PostCustomerById_Test(FileSystemInfo ioFile)
+        {
+            var j = GetIoJObject(ioFile);
+            var uri = GetInputUri(j["input"]["uri"]);
+            var body = JObject.FromObject(j["input"]["body"]);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, uri);
+            request.Headers.Clear();
+            request.Content = new StringContent(body.ToString(), Encoding.UTF8, MimeTypes.ApplicationJson);
+
+            var response = await GetServerResponseAsync(request);
+            j["output"] = JObject.Parse(response);
+
+            File.WriteAllText(ioFile.FullName, j.ToString());
+        }
+
+        /// <summary>
+        /// POSTs an update to the specified customer proprty added
+        /// in <see cref="ElasticSearchTests.PostCustomerById_Test(FileSystemInfo)"/>
+        /// with a painless script.
+        /// </summary>
+        /// <param name="ioFile"></param>
+        [Theory]
+        [ProjectFileData(typeof(ElasticSearchTests),
+            new[]
+            {
+                @"json\PostCustomerPainlessById_Test.json"
+            },
+            numberOfDirectoryLevels: 3)]
+        public async Task PostCustomerPainlessById_Test(FileSystemInfo ioFile)
         {
             var j = GetIoJObject(ioFile);
             var uri = GetInputUri(j["input"]["uri"]);
