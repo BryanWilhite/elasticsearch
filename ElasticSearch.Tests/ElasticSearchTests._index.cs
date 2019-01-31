953df465911c0d299ccdefb4270c5093a14171b3
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-01-30 16:06:07 -0800

    :microscope: PostCustomerById_Test passes

--- ElasticSearch.Tests/ElasticSearchTests._index.cs
+++ ElasticSearch.Tests/ElasticSearchTests._index.cs
@@ -3,7 +3,6 @@ using Songhay.Models;
 using SonghayCore.xUnit;
 using System.IO;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -102,6 +101,36 @@ namespace ElasticSearch.Tests
             },
             numberOfDirectoryLevels: 3)]
         public async Task PostCustomer_Test(FileSystemInfo ioFile)
+        {
+            var j = GetIoJObject(ioFile);
+            var uri = GetInputUri(j["input"]["uri"]);
+            var body = JObject.FromObject(j["input"]["body"]);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, uri);
+            request.Headers.Clear();
+            request.Content = new StringContent(body.ToString(), Encoding.UTF8, MimeTypes.ApplicationJson);
+
+            var response = await GetServerResponseAsync(request);
+            j["output"] = JObject.Parse(response);
+
+            File.WriteAllText(ioFile.FullName, j.ToString());
+        }
+
+        /// <summary>
+        /// POSTs a new customer in an index generated on the fly (when it does not exist).
+        /// </summary>
+        /// <param name="ioFile"></param>
+        /// <remarks>
+        /// Running this POST repeatedly will add multiple documents with auto-generated IDs.
+        /// </remarks>
+        [Theory]
+        [ProjectFileData(typeof(ElasticSearchTests),
+            new[]
+            {
+                @"json\PostCustomerById_Test.json"
+            },
+            numberOfDirectoryLevels: 3)]
+        public async Task PostCustomerById_Test(FileSystemInfo ioFile)
         {
             var j = GetIoJObject(ioFile);
             var uri = GetInputUri(j["input"]["uri"]);
